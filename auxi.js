a8bc39ce0d7cf5903b8e7f31a95f2084c6eb6e29
Lucas Aguiar <de.lucas73@gmail.com>
2017-07-31 14:28:09 -0300

    eventos sucesso

--- auxi.js
+++ auxi.js
@@ -1,11 +1,1 @@
-google.visualization.events.addListener(barras,"select",function(e){
-              //pega o array de itens selecionados
-              var selection = barras.getSelection();
 
-              //varre o array para montar a string que sera exibida
-              var tam = selection.lenght;
-              var str ="teste";
-
-
-          alert(str);
-        });
